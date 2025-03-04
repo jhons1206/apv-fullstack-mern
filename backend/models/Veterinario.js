3a18c890b10368e4c75803606913d51554502664
Jhon Sandoval <webdeveloper84@gmail.com>
2025-03-04 06:28:23 -0500

    Fase 1: Tareas implementadas en el proyecto
    a) Definir Modelo Veterinario, b) Creacion de rutas, c) Incoporar controladores, d) Uso y configuración de Postman para enviar peticiones, e) Leer datos enviados a la API, f) Almacenar registros en la BD, g) Prevenir registro de usuarios duplicados, h) Generar toke unico

---
+++ backend/models/Veterinario.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import generarId from '../helpers/generarId.js'
+
+const veterinarioSchema = mongoose.Schema({
+	nombre: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		requiered: true,
+		unique: true,
+		trim: true,
+	},
+	telefono: {
+		type: String,
+		default: null,
+		trim: true,
+	},
+	web: {
+		type: String,
+		default: null,
+	},
+	token: {
+		type: String,
+		default: generarId(),
+	},
+	confirmado: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+const Veterinario = mongoose.model('Veterinario', veterinarioSchema);
+export default Veterinario;
